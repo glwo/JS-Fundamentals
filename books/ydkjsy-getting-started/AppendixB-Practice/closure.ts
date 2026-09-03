541ec4b48e8123471089a8eb98be6061a9c28cb1
glenwojnar1.0@gmail.com <glenwojnar@yahoo.com>
2026-09-02 22:10:46 -0400

    Practice solves

--- books/ydkjsy-getting-started/AppendixB-Practice/closure.ts
+++ books/ydkjsy-getting-started/AppendixB-Practice/closure.ts
@@ -6,9 +6,9 @@ function range(start, end) {
   // TODO
 }
 
-// range(3, 3); // [3]
-// range(3, 8); // [3, 4, 5, 6, 7, 8]
-// range(3, 0); // []
+range(3, 3); // [3]
+range(3, 8); // [3, 4, 5, 6, 7, 8]
+range(3, 0); // []
 
 var start3 = range(3);
 var start4 = range(4);
