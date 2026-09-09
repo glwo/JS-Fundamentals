0f7f40cd6ba700ef17b324a11391e5fd4044c32a
glenwojnar1.0@gmail.com <glenwojnar@yahoo.com>
2026-09-08 21:30:51 -0400

    Added suggested sloution to closure

--- books/ydkjsy-getting-started/AppendixB-Practice/closure.ts
+++ books/ydkjsy-getting-started/AppendixB-Practice/closure.ts
@@ -41,3 +41,27 @@ start3(8); // [3, 4, 5, 6, 7, 8]
 start3(0); // []
 
 start4(6); // [4, 5, 6]
+
+// Suggested solution
+
+function range(start, end) {
+  start = Number(start) || 0;
+
+  if (end === undefined) {
+    return function getEnd(end) {
+      return getRange(start, end);
+    };
+  } else {
+    end = Number(end) || 0;
+    return getRange(start, end);
+  }
+
+  function getRange(start, end) {
+    var ret = [];
+
+    for (let i = start; i <= end; i++) {
+      ret.push(i);
+    }
+    return ret;
+  }
+}
